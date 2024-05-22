71ba2fe5e47ad4b77f5477a24bc01d7041af4f16
Abu Raihan <aburaihancse066@gmail.com>
2024-05-22 19:28:03 +0600

    Fooditem first section added

--- src/components/Dishes/FoodItem/FoodItem.jsx
+++ src/components/Dishes/FoodItem/FoodItem.jsx
@@ -1,4 +1,4 @@
-import "./style.css"
+import "./style.css";
 import pizza from "../../../assets/Logo.png";
 import bike from "../../../assets/bike.png";
 import { RiDoubleQuotesR } from "react-icons/ri";
@@ -6,53 +6,64 @@ import { RiDoubleQuotesR } from "react-icons/ri";
 const FoodItem = () => {
   return (
     <div className="main">
-        <div className="main-top">
-            <div className="food">
-                <div className="food-image">
-                    <img src={pizza} alt="" />
-                </div>
-                <h5>Pizza</h5>
+      <div className="main-top">
+        <div className="food-container">
+          <div className="food">
+            <div className="food-image">
+              <img src={pizza} alt="" />
             </div>
-            <div className="food">
-                <div className="food-image">
-                    <img src={pizza} alt="" />
-                </div>
-                <h5>Burger</h5></div>
-            <div className="food">
-                <div className="food-image">
-                    <img src={pizza} alt="" />
-                </div>
-                <h5>Shusi</h5></div>
-            <div className="food-text">
-                <p>Find<span>deals</span>, <span>free delivery</span>, and more from our restaurant partners.</p>
-                <img src={bike} alt="bike" />
-
+            <h5>Pizza</h5>
+          </div>
+          <div className="food">
+            <div className="food-image">
+              <img src={pizza} alt="" />
             </div>
+            <h5>Burger</h5>
+          </div>
+          <div className="food">
+            <div className="food-image">
+              <img src={pizza} alt="" />
+            </div>
+            <h5>Shusi</h5>
+          </div>
         </div>
-        <div className="main-bottom">
-       <div className="bottom-quote">
-       <RiDoubleQuotesR className="quote"/>
-            <p className="bottom-text">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</p>
-       </div>
-            <div className="food">
-                <div className="food-image">
-                    <img src={pizza} alt="" />
-                </div>
-                <h5>Pasta</h5>
-            </div>
-            <div className="food">
-                <div className="food-image">
-                    <img src={pizza} alt="" />
-                </div>
-                <h5>Salad</h5></div>
-            <div className="food">
-                <div className="food-image">
-                    <img src={pizza} alt="" />
-                </div>
-                <h5>Desserts</h5></div>
+        <div className="food-text">
+          <p>
+            Find<span>deals</span>, <span>free delivery</span>, and more from
+            our restaurant partners.
+          </p>
+          <img src={bike} alt="bike" />
         </div>
+      </div>
+      <div className="main-bottom">
+        <div className="bottom-quote">
+          <RiDoubleQuotesR className="quote" />
+          <p className="bottom-text">
+            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+          </p>
+        </div>
+        <div className="food">
+          <div className="food-image">
+            <img src={pizza} alt="" />
+          </div>
+          <h5>Pasta</h5>
+        </div>
+        <div className="food">
+          <div className="food-image">
+            <img src={pizza} alt="" />
+          </div>
+          <h5>Salad</h5>
+        </div>
+        <div className="food">
+          <div className="food-image">
+            <img src={pizza} alt="" />
+          </div>
+          <h5>Desserts</h5>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodItem
+export default FoodItem;
